ad9ef1ff34fc90d5732afd45579ba78536c1b703
Joachim Moeyens <moeyensj@gmail.com>
2023-04-27 10:18:21 -0700

    Add test to check that the md5 file correctly packaged and that the md5 hash matches

--- mpc_obscodes/__init__.py
+++ mpc_obscodes/__init__.py
@@ -1,3 +1,4 @@
 from importlib.resources import files
 
 mpc_obscodes = files("mpc_obscodes").joinpath("obscodes_extended.json")
+_mpc_obscodes_md5 = files("mpc_obscodes").joinpath("obscodes_extended.md5")
